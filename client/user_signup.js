48a9e4f07c14e0c71eff4092442552ffc8385d4d
vishalmisra235 <34361144+vishalmisra235@users.noreply.github.com>
2019-12-21 15:24:10 +0530

    Update user_signup.js

--- client/user_signup.js
+++ client/user_signup.js
@@ -1,13 +1,19 @@
-var app = angular.module('myApp', []);
-app.controller('formCtrl', function($scope) {
-    $scope.submit = function() {
-
-      //window.location.replace("http://stackoverflow.com/");
-      alert("Submittted");
-      
-        
+var myApp = angular.module('myApp',[]);
+myApp.controller('userCtrl', function ($scope) {
+    $scope.submit = function(){
+    	var path = window.location.pathname;
+     	var pos = path.lastIndexOf("/");
+     	var p = path.slice(0,pos+1);
+     	var new_path = p+'user_signup.html';
+     	window.location.replace(new_path);
+     	console.log(p);
     };
-    $scope.reset = function(){
-      alert("Reset");
-    }
-});
+    $scope.home = function(){
+    	var path = window.location.pathname;
+     	var pos = path.lastIndexOf("/");
+     	var p = path.slice(0,pos+1);
+     	var new_path = p+'first_page.html';
+     	window.location.replace(new_path);
+     	console.log(p);
+    };
+  });
