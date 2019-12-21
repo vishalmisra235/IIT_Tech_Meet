d15f631894b01ceea061f3f655a7f86d137e11f7
SaiKrupaReddy20 <37209645+SaiKrupaReddy20@users.noreply.github.com>
2019-12-21 14:46:24 +0530

    Add files via upload

--- client/userloginredirect.js
+++ client/userloginredirect.js
@@ -1,14 +1,5 @@
 var myApp = angular.module('myApp',[]);
 myApp.controller('userCtrl', function ($scope) {
-    $scope.submit = function(){
-    	var path = window.location.pathname;
-     	var pos = path.lastIndexOf("/");
-     	var p = path.slice(0,pos+1);
-     	var new_path = p+'user_page.html';
-     	window.location.replace(new_path);
-     	console.log(p);
-    };
-
     $scope.register = function(){
     	var path = window.location.pathname;
      	var pos = path.lastIndexOf("/");
